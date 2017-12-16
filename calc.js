def836bb548af4fc01d4be2531510dee80c3bf8b
Erik Thorelli <ethorelli@gmail.com>
2017-12-16 22:20:46 +0100

    formatting payers' names, dollar amounts and text

--- calc.js
+++ calc.js
@@ -3,12 +3,12 @@ Copyright 2017 Erik Thorelli
 */
 
 var totalBill = prompt("What is the total amount of the bill?");
-// Unneccessary right now -- var payer1 = prompt("What is the first payer's name?"); // TODO: store this for continual use w/o having to re-enter  - #user-friendly
-var income1 = prompt("What is the first payer's annual income? "); // TODO: store this for continual use - #user-friendly
+var payer1 = prompt("What is the first payer's name?"); // TODO: store this for continual use w/o having to re-enter  - #user-friendly
+var income1 = prompt("What is " + payer1 + "'s annual income? "); // TODO: store this for continual use - #user-friendly
 var income1Num = Number(income1); // anxious about the dynamic typing, probably uneccessary
 
-// Unneccessary right now -- var payer2 = prompt("What is the second payer's name?");
-var income2 = prompt("What is the second payer's annual income? ");
+var payer2 = prompt("What is the second payer's name?");
+var income2 = prompt("What is " + payer2 + "'s annual income? ");
 var income2Num = Number(income2);
 
 var totalIncome = income1Num + income2Num;
@@ -17,18 +17,17 @@ var payer1Bill = income1Num / totalIncome * totalBill;
 var payer2Bill = income2Num / totalIncome * totalBill;
 
 
-console.log(payer1Bill);
-console.log(payer2Bill);
+console.log(payer1 + " should pay $" + payer1Bill.toFixed(2));
+console.log(payer2 + " should pay $" + payer2Bill.toFixed(2));
 
-/* 
-//TEST for
+ 
+//TESTS
 var testTotalBill = Boolean(totalBill = payer1Bill + payer2Bill);
 if (testTotalBill = true) {
     console.log(testTotalBill);
 } else {
     console.log("totalBill != payer1Bill + payer2Bill! Uh oh!")
 };
-*/
 
 
 
