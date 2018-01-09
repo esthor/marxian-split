f7a09e4fcdad8a7aa974ed352add1cc4e9d9e977
Erik Thorelli <ethorelli@gmail.com>
2018-01-09 07:59:05 +0100

    init react
    handling install with yarn; in the middle of deciding between a framework or 'vanilla' react to handle form input.

---
+++ marxian-split-react/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import logo from './img/marx.jpg';
+import './App.css';
+import calc from './calc.js';
+
+
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        { calc }
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Payer 1:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default NameForm;
+
+
+
+
+
+
+//OLD TRASH
+
+/* class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+} */
---
+++ pure web/README.md
@@ -0,0 +1,22 @@
+## The Marxian Split - A Communist Bill Splitting Calculator
+A calculator to figure how to split bills among individuals, based on their relative ability to pay.
+
+"Each to their need receives, each to their ability pays."
+
+(Here is a Medium post I wrote about this tool - https://medium.com/unhype/a-marxian-split-460365e079ed)
+
+### How to Try the Calculator
+1. go to http://marx.unhype.me
+
+### How to Run the Calculator from the files
+
+1. Run '/calc.js' code in your developer console
+2. Follow the prompts
+3. Read the console to see the amounts to be paid
+
+### How it works
+1. You enter the total amount of the bill to be split
+2. You enter the total annual income of each of the 2 payers
+3. Karl Marx walks into a bar...
+4. You get the proportionate amount each payer should contribute based on their ability to pay.
+5. Workers of the world unite, and split the bar tab.
