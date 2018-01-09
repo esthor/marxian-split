4d9ce385c13e660ead72cd1dec2106ca771b8f21
Erik Thorelli <ethorelli@gmail.com>
2018-01-09 08:12:05 +0100

    ES6-ify the calc.js code

--- js/calc.js
+++ js/calc.js
@@ -9,27 +9,27 @@ Copyright 2017 Erik Thorelli
 
 "use strict";
 
-var totalBill = prompt("What is the total amount of the bill?");
-var payer1 = prompt("What is the first payer's name?"); // TODO: store this for continual use w/o having to re-enter  - #user-friendly
-var income1 = prompt("What is " + payer1 + "'s annual income? "); // TODO: store this for continual use - #user-friendly
-var income1Num = Number(income1); // anxious about the dynamic typing, probably uneccessary
+let totalBill = prompt("What is the total amount of the bill?");
+let payer1 = prompt("What is the first payer's name?"); // TODO: store this for continual use w/o having to re-enter  - #user-friendly
+let income1 = prompt(`What is ${payer1}'s annual income? `); // TODO: store this for continual use - #user-friendly
+let income1Num = Number(income1); // making sure it's a number
 
-var payer2 = prompt("What is the second payer's name?");
-var income2 = prompt("What is " + payer2 + "'s annual income? ");
-var income2Num = Number(income2);
+let payer2 = prompt("What is the second payer's name?");
+let income2 = prompt(`What is ${payer2}'s annual income?`);
+let income2Num = Number(income2);
 
-var totalIncome = income1Num + income2Num;
+let totalIncome = income1Num + income2Num;
 
-var payer1Bill = income1Num / totalIncome * totalBill;
-var payer2Bill = income2Num / totalIncome * totalBill;
+let payer1Bill = income1Num / totalIncome * totalBill;
+let payer2Bill = income2Num / totalIncome * totalBill;
 
 
-alert(payer1 + " should pay $" + payer1Bill.toFixed(2) +  "\n" + payer2 + " should pay $" + payer2Bill.toFixed(2));
+alert(`${payer1} should pay $${payer1Bill.toFixed(2)}` + "\n" + `${payer2} should pay $${payer2Bill.toFixed(2)}`);
 //alert(payer2 + " should pay $" + payer2Bill.toFixed(2));
 
  
 //TESTS
-var testTotalBill = Boolean(totalBill = payer1Bill + payer2Bill);
+let testTotalBill = Boolean(totalBill = payer1Bill + payer2Bill);
 if (testTotalBill = true) {
     console.log(testTotalBill);
 } else {
